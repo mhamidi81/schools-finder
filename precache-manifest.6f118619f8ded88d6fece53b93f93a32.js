1c19d71fa17e2e45e7de03313a21a090e1bd0a10
mohamed hamidi <mohamed.hamidi@gmail.com>
2019-08-16 18:16:52 +0100

    Updates

---
+++ precache-manifest.6f118619f8ded88d6fece53b93f93a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403424b498643c98403443ae24ed5aa4",
+    "url": "/schools-finder/index.html"
+  },
+  {
+    "revision": "9ecd79fea73a52af1cdf",
+    "url": "/schools-finder/static/css/main.c94aaaab.chunk.css"
+  },
+  {
+    "revision": "260ddb39b050f5a14f9b",
+    "url": "/schools-finder/static/js/2.72290c37.chunk.js"
+  },
+  {
+    "revision": "9ecd79fea73a52af1cdf",
+    "url": "/schools-finder/static/js/main.58f39c40.chunk.js"
+  },
+  {
+    "revision": "dbe7f082d0ac1b8253df",
+    "url": "/schools-finder/static/js/runtime~main.bfd0acdd.js"
+  }
+]);
